9bb12b8d8b10d253a085d524bc5de453f6ba683e
woto <wotoss10@gmail.com>
2021-08-25 19:03:30 -0300

    modelando html ao estudo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,12 +22,7 @@ namespace entity_framework.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
